a810168d72f7b3384b79b1d87eba38074d52a4ac
anuraj <anurajanu1234@gmail.com>
2025-03-16 01:34:51 +0530

    CRUD folder is deleted

---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask, request, render_template, redirect
+from models import db, StudentModel
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/create', methods=['GET', 'POST'])    
+def create():
+    if request.method == 'GET':
+        return render_template('create.html', request=request)
+    
+    if request.method == 'POST':
+        hobby = request.form.getlist('hobbies')
+        hobbies = ",".join(map(str, hobby))
+        first_name = request.form['first_name']
+        second_name = request.form['second_name']
+        email = request.form['email']
+        password = request.form['password']
+        gender = request.form['gender']
+        country = request.form['counry']
+
+        student = StudentModel(
+            first_name=first_name,
+            second_name=second_name,
+            email=email,
+            password=password,
+            gender=gender, 
+            hobbies=hobbies,
+            counry=country
+        )
+        db.session.add(student)
+        db.session.commit()
+        return redirect('/')
+    
+@app.route('/view', methods=['GET'])    
+def DisplayList():
+    students = StudentModel.query.all()
+    return render_template('home.html', students=students, request=request)
+
+@app.route('/delete/<int:id>', methods=['GET', 'POST'])
+def delete(id):
+    student = StudentModel.query.filter_by(id=id).first()
+    if request.method == 'POST':
+        if student:
+            db.session.delete(student)
+            db.session.commit()
+            return redirect('/')
+    return render_template('delete.html', request=request)       
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    student = StudentModel.query.filter_by(id=id).first()
+
+    if request.method == 'POST':
+        db.session.delete(student)
+        db.session.commit()
+        if student:
+            hobby = request.form.getlist('hobbies')
+            hobbies = ",".join(map(str, hobby))
+            first_name = request.form['first_name']
+            second_name = request.form['second_name']
+            email = request.form['email']
+            password = request.form['password']
+            gender = request.form['gender']
+            country = request.form['counry']
+
+            student = StudentModel(
+                first_name=first_name,
+                second_name=second_name,
+                email=email,
+                password=password,
+                gender=gender, 
+                hobbies=hobbies,
+                counry=country
+            )
+            db.session.add(student)
+            db.session.commit()
+            return redirect('/')
+
+    return render_template('edit.html', student=student, request=request) 
+
+@app.route('/')
+def home():
+
+    return render_template('home.html', request = request)
+    
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000, debug=True)
---
+++ models.py
@@ -0,0 +1,28 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class StudentModel(db.Model):
+    __tablename__ = "students"
+
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String())
+    last_name = db.Column(db.String())
+    email = db.Column(db.String())
+    password = db.Column(db.String())
+    gender = db.Column(db.String())
+    hobbies = db.Column(db.String())
+    counry = db.Column(db.String(30))
+
+    def __init__ (self, first_name, second_name, email, password, gender, hobbies, counry):
+
+        self.first_name = first_name
+        self.last_name = second_name
+        self.email = email
+        self.password = password
+        self.gender = gender
+        self.hobbies = hobbies
+        self.counry = counry
+
+        def __repr__(self):
+            return f"(self.first_name):(self.last_name)"
